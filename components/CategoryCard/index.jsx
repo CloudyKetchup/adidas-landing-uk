980626f2768ad4cafe968328693e117d958fe14f
CloudyKetchup <maxdodon25@gmail.com>
2021-03-23 23:56:43 +0000

    removed icons

--- components/CategoryCard/index.jsx
+++ components/CategoryCard/index.jsx
@@ -1,15 +1,12 @@
-import { MdKeyboardArrowRight } from "react-icons/md";
+import styles from "./CategoryCard.module.css";
 
 export default function CategoryCard({ title, imgSrc }) {
 	return (
-		<div className="flex-1 w-full h-full bg-white rounded-md shadow-2xl">
-			<img className="object-cover rounded-t-md h-3/4 w-full" src={imgSrc} />
-			<div className="flex p-3 pl-6 pr-6 space-x-6 h-1/4 w-full">
-				<div className="text-2xl mr-auto">
-					<span>{title}</span>
-				</div>
-				<div className="ml-auto">
-					<MdKeyboardArrowRight size="2.3em" />
+		<div className={`${styles.category__card} flex-1 w-full h-full rounded-md shadow-2xl relative cursor-pointer`}>
+			<div className="absolute z-30 h-full w-full overflow-hidden">
+				<img className="object-cover rounded-md h-full w-full absolute z-0" src={imgSrc} />
+				<div className="bg-white absolute transform rotate-45 p-10 pl-4 pt-4 -left-16 -bottom-28 z-0" style={{ height: "70%", width: "70%" }}>
+					<span className="text-bolder text-md">{title}</span>
 				</div>
 			</div>
 		</div>
